63dfb3b4c4fe3a1008fac86898363c293856fcdc
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2026-03-09 06:27:10 +0200

    feat: implement layout & styling

--- src/layout/app-layout.ts
+++ src/layout/app-layout.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { History, House, LucideAngularModule, LucideIconData } from 'lucide-angular';
+
+interface AppTab {
+  label: string;
+  icon: LucideIconData;
+  route: string;
+}
 
 @Component({
   selector: 'app-layout',
   templateUrl: 'app-layout.html',
   styleUrl: 'app-layout.scss',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, LucideAngularModule, RouterLinkActive],
 })
-export class AppLayoutComponent {}
+export class AppLayoutComponent {
+  protected readonly appTabs: AppTab[] = [
+    {
+      label: 'Home',
+      icon: House,
+      route: '/',
+    },
+    {
+      label: 'History',
+      icon: History,
+      route: '/history',
+    },
+  ];
+  protected readonly currentYear = new Date().getFullYear();
+}
