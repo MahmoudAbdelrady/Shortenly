bc066bfecdab0f7899fafc9c851671050dd8a63f
MahmoudAbdelrady <mahmoudabdelrady876@gmail.com>
2026-03-14 11:07:09 +0200

    feat: add backend files

---
+++ backend/src/main/java/com/mdevs/shortenly/ShortenlyApplication.java
@@ -0,0 +1,13 @@
+package com.mdevs.shortenly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortenlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortenlyApplication.class, args);
+	}
+
+}
