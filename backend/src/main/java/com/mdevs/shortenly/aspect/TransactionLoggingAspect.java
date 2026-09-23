62926f38643374aa8274a8f75c66a5dd9910e709
MahmoudAbdelrady <112890203+MahmoudAbdelrady@users.noreply.github.com>
2026-09-23 13:38:45 +0300

    refactor: code enhancement (#10)
    * feat: add logging aspects for request and transaction handling
    
    * chore: update dependencies and enhance health checks in Docker Compose
    
    * chore: bump version to 1.0.1 in pom.xml
    
    * feat: enhance CI/CD workflow with Docker image versioning and deployment steps

---
+++ backend/src/main/java/com/mdevs/shortenly/aspect/TransactionLoggingAspect.java
@@ -0,0 +1,27 @@
+package com.mdevs.shortenly.aspect;
+
+import com.mdevs.shortenly.utils.LoggingUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Aspect
+@Component
+@Slf4j
+public class TransactionLoggingAspect {
+
+    @Around("@annotation(org.springframework.transaction.annotation.Transactional)")
+    public Object logAroundTransactionMethods(ProceedingJoinPoint joinPoint) throws Throwable {
+        String declaringClass = joinPoint.getSignature().getDeclaringTypeName();
+        String methodName = joinPoint.getSignature().getName();
+
+        String logInfo = "Transaction with method: {}.{}";
+        List<Object> logArgs = List.of(declaringClass, methodName);
+
+        return LoggingUtil.proceedWithLogging(joinPoint, log, logInfo, logArgs);
+    }
+}
